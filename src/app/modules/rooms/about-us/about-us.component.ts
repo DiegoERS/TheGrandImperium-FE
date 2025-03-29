5c7b865f899cc2bac98649857322004d66d153b2
NelVargas07 <vargascalderonnelson@gmail.com>
2025-03-29 09:26:45 -0600

    Animacion del carrusel  y redireccion del anuncio

--- src/app/modules/rooms/about-us/about-us.component.ts
+++ src/app/modules/rooms/about-us/about-us.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { AdvertisingComponent } from "../../../shared/components/advertising/advertising.component";
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
-  imports: [AdvertisingComponent, RouterModule],
+  imports: [RouterModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
