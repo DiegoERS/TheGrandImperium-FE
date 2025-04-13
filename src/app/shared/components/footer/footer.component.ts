a1821cd4f3cb60ad9f6a6365c7d7275570a594c4
YeisonMonteroVillalobos16 <yeisonmonterovillalobos@gmail.com>
2025-04-13 15:18:30 -0600

    Added menu admin and button log out

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import {RouterModule} from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-footer',
   imports: [RouterModule],
@@ -7,5 +8,19 @@ import {RouterModule} from '@angular/router';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  private router= inject(Router);
+  logout(): void {
+   if(localStorage.getItem("user")){
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('user');
+    this.router.navigate(['/login']);
+  }else{
+    alert("No hay un usuario logeado")
+  }
+
+  }
+
 
 }
