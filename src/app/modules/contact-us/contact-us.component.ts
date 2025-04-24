e56f116ab7bdb5eb718f6b8d8d3cd9e81454179e
NelVargas07 <vargascalderonnelson@gmail.com>
2025-04-24 14:39:55 -0600

    Dinamico contact-us and how to get to

--- src/app/modules/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { PageInformationService } from '../../core/services/page-information.service';
+import { PageInformationDTO } from '../../core/models/PageInformationDTO';
+
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
+
+  pageInformation: PageInformationDTO | null = null;
+  descriptions: string[] = [];
+  private pageInformationService = inject(PageInformationService);
+
+
+  ngOnInit(): void {
+    var page = JSON.parse(localStorage.getItem('selectedPage') || '{}');
+    this.pageInformationService.getByPage(page.pageId).subscribe((data: PageInformationDTO) => {
+      this.pageInformation = data;
+      this.descriptions = data.description.split('^').map(item => item.trim());
+      for (let i = 0; i < this.descriptions.length; i++) {
+        if (this.descriptions[i].includes('&#64')) {
+          this.descriptions[i] = this.descriptions[i].replace('&#64;', '@');
+        }
+      }
+    });
+  }
+
+  getIcon(item: string): string {
+    if (item.includes('Hotel')) {
+      return 'fa-hotel';
+    } else if (item.includes('Tel')) {
+      return 'fa-phone';
+    } else if (item.includes('@')) {
+      return 'fa-envelope';
+    } else if (item.includes('Apdo Postal')) {
+      return 'fa-map-marker-alt';
+    }
+    return 'fa-info-circle';
+  }
 
 }
