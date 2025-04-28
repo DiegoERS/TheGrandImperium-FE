ea501fa4aa7df507f395522dcac8d79623192e08
DiegoERS <diegoerios10@gmail.com>
2025-04-27 22:27:17 -0600

    implementacion de spinners, arreglos varios y paginacion

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -2,15 +2,17 @@ import { Component,inject,OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageInformationService } from '../../core/services/page-information.service';
 import { PageInformationDTO } from '../../core/models/PageInformationDTO';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   private pageId: number = 1;
+  loading = true;
   private pageInformationService=inject(PageInformationService);
   pageInformation: PageInformationDTO | null = null;
   images: string[] = [];
@@ -21,6 +23,7 @@ export class HomeComponent implements OnInit {
       this.pageInformation = data;
       this.images = data.pageInformationImageDTOs.map(imgDTO => imgDTO.imageDTO.url);
        this.selectedImage = this.images.length > 0 ? this.images[0] : null;
+       this.loading = false;
     });
   }
 }
